ac766ff693584319ffdf53f34acc69838d5e0125
jcny4747 <85555675+jcny4747@users.noreply.github.com>
2021-06-22 20:36:39 +0800

    Create stairs.js

---
+++ stairs.js
@@ -0,0 +1,14 @@
+var lines = 6
+for (var i = 0; i < lines; i ++){
+    for (var j = 0; j <= i; j++){
+      document.write("#")
+    }
+  document.write("</br>")
+}
+
+for (var i = 0; i < lines; i ++){ 
+  for (var j = 0; j < lines-i-1; j++){
+  document.write("#")
+  }
+  document.write("</br>")
+}
